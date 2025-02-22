536205f668bf214858b6b4e6d7e9891ec87df3df
pranavps1122 <pranavps01@gmail.com>
2025-02-22 11:30:44 +0530

    b

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../model/userSchema");
 
 
+
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error('Google OAuth credentials missing. Check your .env file.');
 }
@@ -14,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID.trim(),
       clientSecret: process.env.GOOGLE_CLIENT_SECRET.trim(),
-      callbackURL: "https://www.kuppayam.online/auth/google/callback",
+      callbackURL: "https://kuppayam.online/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
