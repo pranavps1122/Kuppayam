19891ca86d9875e9aec77f0f4d2f7274f232bc6f
pranavps1122 <pranavps01@gmail.com>
2025-03-01 10:24:50 +0530

    new chnages

--- config/passport.js
+++ config/passport.js
@@ -21,14 +21,14 @@ passport.use(
     async (req, accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ email: profile.emails[0].value });
-
+        
         if (user) {
+          if (user.Status === false) {
+            return done(null, false, { message: "Account is inactive" });
+          }
           user.name = profile.displayName;
           await user.save();
           return done(null, user);
-        }
-        if(user.Status===false){
-          return res.redirect('/login')
         }
 
         user = new User({
